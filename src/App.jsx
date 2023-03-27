e91a9fd4e49a5b62a46f9a7414c62dcff84f5e0a
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-03-27 17:14:26 +0600

    Footer code added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
 import Shop from './components/Shop/Shop'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Shop></Shop>
+      <Footer></Footer>
     </div >
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div>
+            <h6 className='footer'>Copyright &copy;Ema-John-Shop 2023 Allright Reserved</h6>
+        </div>
+    );
+};
+
+export default Footer;
