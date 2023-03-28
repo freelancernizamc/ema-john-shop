215b7269652af89f60371449e819a918a73ead56
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-03-28 15:57:40 +0600

    show data from cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -13,10 +14,35 @@ const Shop = () => {
 
     }, []);
 
+    useEffect(() => {
+        // console.log('products', products);
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        //step 1: get id of the addedProduct
+        for (const id in storedCart) {
+            // console.log(id);
+            // step 2: get the product by using id
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                // step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the added product to the saved cart  
+                savedCart.push(addedProduct);
+            }
+
+
+            console.log(addedProduct);
+        }
+        // step 5: set the cart
+        setCart(savedCart);
+    }, [products])
+
     const handleAddToCart = (product) => {
         // console.log(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
     return (
         <div className='shop-container'>
